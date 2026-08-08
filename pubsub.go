46ac672251e062053e1b78c6f049aef4da31347d
Kirill Zuyeu <kirillzuyeu@gmail.com>
2026-08-08 15:46:11 +0300

    feat: registerHandlers creates queues

--- pubsub.go
+++ pubsub.go
@@ -6,8 +6,26 @@ import (
 	rmq "github.com/rabbitmq/rabbitmq-amqp-go-client/pkg/rabbitmqamqp"
 )
 
+/*
+@todo: implement Close function
+
+defer pub.Close()
+
+- conn.Close()
+- publisher.Close()
+*/
 type Pubsub struct {
 	conn *rmq.AmqpConnection
+	ctx  context.Context
+}
+
+type Message struct {
+	Message string
+}
+
+type Handler interface {
+	Queue() string
+	Handle(Message)
 }
 
 func New(ctx context.Context, address string) (*Pubsub, error) {
@@ -18,5 +36,25 @@ func New(ctx context.Context, address string) (*Pubsub, error) {
 		return &Pubsub{}, err
 	}
 
-	return &Pubsub{conn}, nil
+	return &Pubsub{conn: conn, ctx: ctx}, nil
+}
+
+/*
+This thing should:
+
+- create neccessary queues
+- register recievers as handlers(idk, LOL)
+*/
+func (p *Pubsub) RegisterHandlers(handlers ...Handler) error {
+	for _, handler := range handlers {
+		_, err := p.conn.Management().DeclareQueue(
+			p.ctx,
+			&rmq.QuorumQueueSpecification{Name: handler.Queue()},
+		)
+
+		if err != nil {
+			return err
+		}
+	}
+	return nil
 }
