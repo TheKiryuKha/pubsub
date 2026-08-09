a67f51d308eed833bae40e3ff2953d13df4b8e90
Kirill Zuyeu <kirillzuyeu@gmail.com>
2026-08-09 14:46:11 +0300

    feat: gracefull shutdown

--- pubsub.go
+++ pubsub.go
@@ -8,6 +8,8 @@ import (
 	"errors"
 	"fmt"
 	"log"
+	"sync"
+	"time"
 
 	rmq "github.com/rabbitmq/rabbitmq-amqp-go-client/pkg/rabbitmqamqp"
 )
@@ -17,6 +19,9 @@ const ExchangeName string = "main"
 type Pubsub struct {
 	conn *rmq.AmqpConnection
 	bus  *rmq.Publisher
+
+	cancelConsumers context.CancelFunc
+	wg              sync.WaitGroup
 }
 
 type Event struct {
@@ -27,7 +32,7 @@ type Event struct {
 type Handler interface {
 	Queue() string
 	Events() []string
-	Handle(Event) error
+	Handle(context.Context, Event) error
 }
 
 func New(ctx context.Context, address string) (*Pubsub, error) {
@@ -62,6 +67,17 @@ This thing should:
 - register recievers as handlers(idk, LOL)
 */
 func (p *Pubsub) RegisterHandlers(ctx context.Context, handlers ...Handler) error {
+	consumerCtx, cancel := context.WithCancel(ctx)
+	p.cancelConsumers = cancel
+
+	success := false
+	defer func() {
+		if !success {
+			cancel()
+			p.wg.Wait()
+		}
+	}()
+
 	for _, handler := range handlers {
 		// create queue
 		qInfo, err := p.conn.Management().DeclareQueue(
@@ -90,8 +106,18 @@ func (p *Pubsub) RegisterHandlers(ctx context.Context, handlers ...Handler) erro
 			return err
 		}
 
-		go func(consumer *rmq.Consumer, handler Handler) {
-			defer consumer.Close(ctx)
+		p.wg.Add(1)
+		go func(consumer *rmq.Consumer, handler Handler, ctx context.Context) {
+			defer p.wg.Done()
+			defer func() {
+				cancelCtx, cancel := context.WithTimeout(
+					context.Background(),
+					5*time.Second,
+				)
+				defer cancel()
+
+				consumer.Close(cancelCtx)
+			}()
 
 			for {
 				delivery, err := consumer.Receive(ctx)
@@ -99,8 +125,8 @@ func (p *Pubsub) RegisterHandlers(ctx context.Context, handlers ...Handler) erro
 					if errors.Is(err, context.Canceled) {
 						return
 					}
-					log.Printf("%v", err)
-					continue
+					log.Printf("%v. Queue: %s. Cancel Reciever", err, handler.Queue())
+					return
 				}
 
 				msg := delivery.Message().Data[0]
@@ -113,16 +139,26 @@ func (p *Pubsub) RegisterHandlers(ctx context.Context, handlers ...Handler) erro
 					continue
 				}
 
-				err = handler.Handle(event)
+				err = handler.Handle(ctx, event)
 				if err != nil {
 					delivery.Requeue(ctx)
 					continue
 				}
 
-				delivery.Accept(ctx)
+				err = delivery.Accept(ctx)
+				if err != nil {
+					if errors.Is(err, context.Canceled) {
+						return
+					}
+					log.Printf("%v. Queue: %s. Failed to accept delivery. Close reciever", err, handler.Queue())
+					return
+				}
 			}
-		}(consumer, handler)
+		}(consumer, handler, consumerCtx)
 	}
+
+	success = true
+
 	return nil
 }
 
@@ -154,7 +190,21 @@ func (p *Pubsub) Dispatch(ctx context.Context, event Event) error {
 	return nil
 }
 
-func (p *Pubsub) Close(ctx context.Context) {
-	_ = p.bus.Close(ctx)
-	_ = p.conn.Close(ctx)
+func (p *Pubsub) Close(ctx context.Context) error {
+	var errs []error
+
+	if p.cancelConsumers != nil {
+		p.cancelConsumers()
+		p.wg.Wait()
+	}
+
+	if err := p.bus.Close(ctx); err != nil {
+		errs = append(errs, fmt.Errorf("Failed to close publisher, %v", err))
+	}
+
+	if err := p.conn.Close(ctx); err != nil {
+		errs = append(errs, fmt.Errorf("Failed to close connection, %v", err))
+	}
+
+	return errors.Join(errs...)
 }
